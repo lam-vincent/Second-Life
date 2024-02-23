b2d9d87a0b20d573fa361115ef660f9d1c9e3b97
lam-vincent <mrccniv@gmail.com>
2024-02-23 22:06:36 +0100

    add: dependencies and modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { IncidentsModule } from './incidents/incidents.module';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, IncidentsModule, DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
