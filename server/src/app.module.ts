6384e4cef5a2a4de451266586e22540fc9b28c1d
lam-vincent <mrccniv@gmail.com>
2024-02-25 23:59:53 +0100

    upd: database configuration in app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -11,14 +12,14 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres', 
       host: 'localhost',
-      port: 3306,
+      port: 5432, 
       username: 'root',
       password: 'root',
       database: 'test',
-      entities: [],
-      synchronize: true,
+      entities: [], 
+      synchronize: true, // This is for development only, set to false in production
     }),
     AuthModule,
     IncidentsModule,
