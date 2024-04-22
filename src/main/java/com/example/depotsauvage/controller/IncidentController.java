5ca99da139af0d1d07040723a5b3787f6a04764c
lam-vincent <mrccniv@gmail.com>
2024-04-22 09:51:27 +0200

    Add endpoint for saying hello in IncidentController.java

--- src/main/java/com/example/depotsauvage/controller/IncidentController.java
+++ src/main/java/com/example/depotsauvage/controller/IncidentController.java
@@ -1,5 +1,13 @@
 package com.example.depotsauvage.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IncidentController {
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
